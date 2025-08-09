66a0e0d39b569d0aa83e07ea2d3c96f9bdf879f5
Gustavo <luizsilvagf98@gmail.com>
2025-08-08 23:14:35 -0300

    Adicionando novas funções no código sobre fatorial

--- Python/debug.py
+++ Python/debug.py
@@ -1,1 +1,17 @@
-print('Olá, mundo!!!')
+from math import factorial
+
+def map_factorial(numbers):
+    result = []
+
+    for num in numbers:
+        result.append(factorial(num))
+    
+    return result
+
+def main():
+    input_list = [1,2,3,4]
+    return map_factorial(input_list)
+
+
+if __name__ == "__main__":
+    main()
